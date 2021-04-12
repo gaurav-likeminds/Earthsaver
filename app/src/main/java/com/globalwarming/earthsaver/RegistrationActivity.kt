d86019577ad93dcf3555b6a842a098e7f900a958
Gaurav LikeMinds <gaurav@likeminds.community>
2021-04-13 02:56:19 +0530

    upload profile

--- app/src/main/java/com/globalwarming/earthsaver/RegistrationActivity.kt
+++ app/src/main/java/com/globalwarming/earthsaver/RegistrationActivity.kt
@@ -2,16 +2,20 @@ package com.globalwarming.earthsaver
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Patterns
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.globalwarming.earthsaver.databinding.ActivityRegistrationBinding
 import com.globalwarming.earthsaver.util.Loader
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import java.util.*
+
 
 class RegistrationActivity : AppCompatActivity() {
 
@@ -19,6 +23,9 @@ class RegistrationActivity : AppCompatActivity() {
     private lateinit var loader: Loader
     private lateinit var mAuth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
+
+    private var imageUrl: String? = null
+    private val PICK_IMAGE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +40,20 @@ class RegistrationActivity : AppCompatActivity() {
         mAuth = FirebaseAuth.getInstance()
         db = FirebaseFirestore.getInstance()
         loader = Loader(this, "Registering ...")
+
+        binding.profile.setOnClickListener {
+            val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+            getIntent.type = "image/*"
+
+            val pickIntent =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            pickIntent.type = "image/*"
+
+            val chooserIntent = Intent.createChooser(getIntent, "Select Image")
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(pickIntent))
+
+            startActivityForResult(chooserIntent, PICK_IMAGE)
+        }
 
         binding.button.setOnClickListener { v ->
             Util.hideKeyboard(v)
@@ -67,6 +88,9 @@ class RegistrationActivity : AppCompatActivity() {
                             dataToUpload["age"] = finalAge
                             dataToUpload["gender"] = gender
                             dataToUpload["location"] = location
+                            if (imageUrl != null) {
+                                dataToUpload["image"] = imageUrl!!
+                            }
                             db.collection("users")
                                 .document(uid)
                                 .set(dataToUpload)
@@ -89,8 +113,11 @@ class RegistrationActivity : AppCompatActivity() {
                                 }
                         } else {
                             loader.dismiss()
-                            Snackbar.make(v, task.exception?.message.toString(), Snackbar.LENGTH_LONG)
-                                .show()
+                            Snackbar.make(
+                                v,
+                                task.exception?.message.toString(),
+                                Snackbar.LENGTH_LONG
+                            ).show()
                         }
                     }
             }
@@ -138,6 +165,35 @@ class RegistrationActivity : AppCompatActivity() {
             return false
         }
         return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            val uri = data.data ?: return
+            loader.setText("Uploading profile image ...")
+            loader.show()
+            val storageRef = FirebaseStorage.getInstance().reference
+                .child("users")
+                .child("${System.currentTimeMillis()}.png")
+            val uploadTask = storageRef.putFile(uri)
+            uploadTask.continueWithTask { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                storageRef.downloadUrl
+            }.addOnCompleteListener { task ->
+                loader.dismiss()
+                if (task.isSuccessful) {
+                    imageUrl = task.result.toString()
+                    Glide.with(binding.profile).load(uri).into(binding.profile)
+                } else {
+                    Snackbar.make(binding.root, "Unable to upload image", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
